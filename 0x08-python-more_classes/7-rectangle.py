bf217e6d6e2a83cad28610c958b8bcd995ce965d
Ccinaza <ccinaza@gmail.com>
2023-01-02 20:57:36 -0800

    More classes

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -2,6 +2,7 @@
 # 7-rectangle.py
 """Defines a Rectangle class."""
 
+
 class Rectangle:
     """Represent a rectangle.
     Attributes:
@@ -71,8 +72,6 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
-
-
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
